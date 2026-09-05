da1364cdbc1860db0a33ba146d0f9b98e765b937
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2026-09-05 17:42:17 +0100

    Generic Genetic Algorithm
    - Created a generic genetic algorithm for all manner of tasks
    - Basic < or > algorithm

---
+++ GeneticModelBase.h
@@ -0,0 +1,185 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+/*
+My goal is to make a generic neuroevolving model base, which can be adapted for whatever else I want to
+do.
+*/
+
+class Perceptron;
+
+class Edge {
+public:
+	Perceptron* start;
+	Perceptron* end;
+	double weight = 0.1;
+
+	Edge(Perceptron* startNode, Perceptron* endNode, double w)
+		: start(startNode), end(endNode), weight(w) {
+	}
+};
+
+class Perceptron {
+public:
+	vector<unique_ptr<Edge>> edges = {};
+	double bias = 0.1;
+	double result = 0.1;
+	string activationFunc;
+
+	Perceptron(string activation) : activationFunc(activation) {}
+
+	void ActivationFunction();
+
+private:
+	double CalculateSum();
+};
+
+class Model {
+public:
+	vector<vector<unique_ptr<Perceptron>>> layers = {}; //[0] is input layer, [numLayers - 1] is output layer
+	int GetNumLayers() const {
+		return layers.size();
+	}
+
+	vector<Perceptron*> rawPerceptrons = {};
+	vector<Edge*> rawEdges = {};
+
+	vector<double> RunAlgorithm(vector<double> inputs);
+
+	void AddDenseLayer(int numNodes, string activationFunction);
+
+	void Crossover(Model* otherModel, double mutationChance, double mutationSD);
+
+	void GenerateRawList();
+
+	unique_ptr<Model> Clone();
+};
+
+using namespace std;
+
+random_device rd;
+mt19937 gen(rd());
+normal_distribution<double> norm(0.0, 1.0);
+uniform_real_distribution<double> uniform(0.0, 1.0);
+
+double Perceptron::CalculateSum() {
+	double sum = bias;
+
+	for (const auto& edge : edges) sum += edge->start->result * edge->weight;
+
+	return sum;
+}
+
+void Perceptron::ActivationFunction() {
+	double out = 0;
+
+	double sum = CalculateSum();
+	if (activationFunc == "tanh") out = tanh(sum);
+	else if (activationFunc == "sigmoid") out = 1 / (1 + exp(-sum));
+
+	else cout << "Activation function " << activationFunc << " unknown";
+
+	result = out;
+}
+
+vector<double> Model::RunAlgorithm(vector<double> inputs) {
+	auto& inputLayer = layers[0];
+
+	for (int i = 0; i < inputLayer.size(); i++) inputLayer[i]->result = inputs[i];
+
+	for (int i = 1; i < layers.size(); i++) {
+		auto& targetLayer = layers[i];
+		for (int j = 0; j < targetLayer.size(); j++) targetLayer[j]->ActivationFunction();
+	}
+
+	vector<double> out = {};
+	for (const auto& perceptron : layers.back()) out.push_back(perceptron->result);
+
+	return out;
+}
+
+void Model::AddDenseLayer(int numPerceptrons, string activationFunction) {
+	vector<unique_ptr<Perceptron>> newLayer;
+
+	for (int i = 0; i < numPerceptrons; i++) {
+		auto perceptron = make_unique<Perceptron>(activationFunction);
+		perceptron->bias = norm(gen);
+
+		if (!layers.empty()) {
+			auto& previousLayer = layers.back();
+			for (const auto& previousPerceptron : previousLayer) {
+				double randomWeight = norm(gen);
+				perceptron->edges.push_back(make_unique<Edge>(previousPerceptron.get(), perceptron.get(), randomWeight));
+
+			}
+		}
+
+		newLayer.push_back(move(perceptron));
+	}
+
+	layers.push_back(move(newLayer));
+}
+
+void Model::Crossover(Model* otherModel, double mutationChance, double mutationSD) {
+	normal_distribution<double> mutator(0.0, mutationSD);
+
+	for (int i = 0; i < rawPerceptrons.size(); i++) {
+		if (uniform(gen) <= 0.5) rawPerceptrons[i]->bias = otherModel->rawPerceptrons[i]->bias;
+		if (uniform(gen) <= mutationChance) rawPerceptrons[i]->bias += mutator(gen);
+	}
+
+	for (int i = 0; i < rawEdges.size(); i++) {
+		if (uniform(gen) <= 0.5) rawEdges[i]->weight = otherModel->rawEdges[i]->weight;
+		if (uniform(gen) <= mutationChance) rawEdges[i]->weight += mutator(gen);
+	}
+}
+
+void Model::GenerateRawList() {
+	rawPerceptrons.clear();
+	rawEdges.clear();
+
+	for (auto& layer : layers) {
+		for (auto& perceptron : layer) {
+			rawPerceptrons.push_back(perceptron.get());
+
+			for (auto& edge : perceptron->edges) rawEdges.push_back(edge.get());
+		}
+
+	}
+}
+
+unique_ptr<Model> Model::Clone() {
+	//cout << "Clone 1 " << endl;
+
+	auto clonedModel = make_unique<Model>();
+
+	for (const auto& layer : layers) {
+		if (layer.empty()) continue;
+
+		string activationFunc = layer[0]->activationFunc;
+		clonedModel->AddDenseLayer(layer.size(), activationFunc);
+	}
+
+	clonedModel->GenerateRawList();
+
+	//cout << "Clone 2 " << endl;
+
+	//cout << rawEdges.size() << endl;
+
+	for (size_t i = 0; i < rawEdges.size(); i++) {
+		clonedModel->rawEdges[i]->weight = rawEdges[i]->weight;
+	}
+
+	//cout << "Clone 3" << endl;
+
+	for (size_t i = 0; i < rawPerceptrons.size(); i++) {
+		clonedModel->rawPerceptrons[i]->bias = rawPerceptrons[i]->bias;
+	}
+	return clonedModel;
+}
--- GeneticRouting.cpp
+++ GeneticRouting.cpp
@@ -1,27 +1,119 @@
 ﻿// GeneticRouting.cpp : Defines the entry point for the application.
 //
 
-#include "GeneticRouting.h"
-//#include "ModelBase.cpp"
+#include "GeneticModelBase.h"
 
 using namespace std;
 
+//settings
+int NUM_OF_MODELS = 100;
+int NUM_MODELS_FOR_CROSSOVER = 10;
+double MUTATION_CHANCE = 0.05;
+double MUTATION_SD = 0.01;
+int NUM_OF_ITERATIONS = 1000;
+
+random_device random;
+mt19937 generator(random());
+uniform_int_distribution uniformInt(0, NUM_MODELS_FOR_CROSSOVER - 1);
+
 int main()
 {
-	Model model;
+	vector<unique_ptr<Model>> models;
+	vector<tuple<int, double>> outputs = {};
 
-	//Making a 2-8-1 model
+	// Making 100 models (2-3-1 architecture)
+	for (int i = 0; i < NUM_OF_MODELS; i++) {
+		auto model = make_unique<Model>();
 
-	model.AddDenseLayer(2, "tanh");
-	model.AddDenseLayer(8, "tanh");
-	model.AddDenseLayer(1, "tanh");
+		model->AddDenseLayer(2, "tanh");
+		model->AddDenseLayer(3, "tanh");
+		model->AddDenseLayer(1, "sigmoid");
+
+		model->GenerateRawList();
+
+		models.push_back(std::move(model));
+		outputs.push_back({ i, 0 });
+	}
 
 
-	vector<double> inputs = {0.4, 0.3};
-	vector<double> out = model.RunAlgorithm(inputs);
+	//For this genetic experiment, the model will take in 2 numbers between 0 and 1 ,a and b.
+	//Output 0 if a < b, output 1 if a >= b
 
-	cout << out[0] << endl;
+	vector<tuple<double, double, double>> inputs = {};
 
-	cout << "Hello CMake." << endl;
+	double res;
+	for (double i = 0.01; i < 1; i += 0.01) {
+		for (double j = 0.01; j < 1; j += 1) {
+			if (i < j) res = 0;
+			else res = 1;
+
+			inputs.push_back({i, j, res});
+		}
+	}
+
+	for (int i = 0; i < NUM_OF_ITERATIONS; i++) {
+		if ((i + 1) % 10 == 0) cout << "Iterations : " << i + 1 << "/" << NUM_OF_ITERATIONS << endl;
+
+		for (int i = 0; i < outputs.size(); i++) {
+			get<0>(outputs[i]) = i;
+		}
+
+		for (int j = 0; j < models.size(); j++) {
+			double offset = 0;
+
+			for (tuple<double, double, double> set : inputs) {
+				vector<double> modelInputs = { get<0>(set), get<1>(set) };
+				double out = models[j]->RunAlgorithm(modelInputs)[0];
+
+				offset += fabs(get<2>(set) - out);
+			}
+
+			get<1>(outputs[j]) = offset;
+		}
+
+		//Evolution phase
+		sort(outputs.begin(), outputs.end(),
+			[](const tuple<int, double>& a, const tuple<int, double>& b) { return get<1>(a) < get<1>(b); }
+		);
+
+		//cout << "Most effective score : " << get<1>(outputs[0]) << endl;
+
+		vector<unique_ptr<Model>> newGeneration = {};
+
+		int bestModelIdx = get<0>(outputs[0]);
+
+		//cout << "Best model index : " << bestModelIdx << endl;
+
+		newGeneration.push_back(models[bestModelIdx]->Clone());
+
+		//cout << outputs.size() << " Outputs size " << endl;
+
+		while (newGeneration.size() < NUM_OF_MODELS) {
+			int parent1Idx = get<0>(outputs[uniformInt(generator)]);
+			int parent2Idx = get<0>(outputs[uniformInt(generator)]);
+
+			while (parent2Idx == parent1Idx && NUM_MODELS_FOR_CROSSOVER > 1) {
+				parent2Idx = get<0>(outputs[uniformInt(generator)]);
+			}
+
+			auto child = models[parent1Idx]->Clone();
+
+			child->Crossover(models[parent2Idx].get(), MUTATION_CHANCE, MUTATION_SD);
+			child->GenerateRawList();
+
+			newGeneration.push_back(std::move(child));
+		}
+
+		models = std::move(newGeneration);
+	}
+
+	sort(outputs.begin(), outputs.end(),
+		[](const tuple<int, double>& a, const tuple<int, double>& b) { return get<1>(a) < get<1>(b); }
+	);
+
+	cout << "Most effective score : " << get<1>(outputs[0]) << endl;
+
+	//for (auto output : outputs) cout << get<1>(output) << endl;
+
 	return 0;
 }
